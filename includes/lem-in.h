97c9f3fdade565518476b9736398d068e4d1a4d2
Clurquiz <clementine.urquizar@gmail.com>
2018-04-30 20:37:14 +0200

    add first architecture

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -3,4 +3,28 @@
 
 # include "libft.h"
 
+typedef struct	s_room
+{
+	int		num;
+	char	*name;
+	int		weight;
+	// int		adj_number;
+}				t_room;
+
+typedef struct	s_ant
+{
+	int		name;
+	t_room	*room;
+	// t_room	*previous_room;
+}				t_ant;
+
+typedef struct	s_graph
+{
+	int		ants_number;
+	t_ant	**ants;
+	t_room	**rooms;
+	int		**adj_matrix;
+
+}				t_graph;
+
 #endif

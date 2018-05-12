62a5a1e03aeb080bb1e7e7114b425512e9bfcca7
Clementine URQUIZAR <curquiza@e2r9p19.42.fr>
2018-05-12 18:19:44 +0200

    wip

--- srcs/main.c
+++ srcs/main.c
@@ -7,7 +7,7 @@ int		is_comment(char *line)
 
 int		is_valid_command(char *line)
 {
-	return ( is_start_command(line) || is_end_command(line))
+	return (is_start_command(line) || is_end_command(line))
 }
 
 int		is_start_command(char *line)
@@ -20,25 +20,60 @@ int		is_end_command(char *line)
 	return (strequ(line, "##end"));
 }
 
-int		parser(void)
+int		str_is_digit(char *str)
+{
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+	}
+	return (1);
+}
+
+int		get_ants_number(char *line)
 {
 	char	*line;
-	int		ret;
 
+	line = NULL;
 	while (42)
 	{
-		ret = -2;
-		if ((ret = get_next_line(0, &line)) != 1)
+		if (get_next_line(0, &line) != 1 || !line)
 		{
 			ft_strdel(&line);
+			return (-1);
+		}
+		if (is_valid_command(line))
+		{
+			ft_strdel(&line);
+			return (-1);
+		}
+		if (str_is_digit(line))
+		{	
+			
 			return (0);
 		}
-		else
-		{
-			ft_putendl(line);
-			ft_strdel(&line);
-		}
 	}
+}
+
+int		parser(void)
+{
+	// char	*line;
+	// int		ret;
+
+	// while (42)
+	// {
+	// 	ret = -2;
+	// 	if ((ret = get_next_line(0, &line)) != 1)
+	// 	{
+	// 		ft_strdel(&line);
+	// 		return (0);
+	// 	}
+	// 	else
+	// 	{
+	// 		ft_putendl(line);
+	// 		ft_strdel(&line);
+	// 	}
+	// }
 	return (0);
 }
 

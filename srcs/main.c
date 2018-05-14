8cd9bed9d142ed53c9a0be97177e5ca506337aa3
Clurquiz <clementine.urquizar@gmail.com>
2018-05-14 23:42:16 +0200

    add get number of ants

--- srcs/main.c
+++ srcs/main.c
@@ -1,23 +1,23 @@
 #include "lem-in.h"
 
-int		is_comment(char *line)
-{
-	return (line[0] == '#');
-}
-
-int		is_valid_command(char *line)
-{
-	return (is_start_command(line) || is_end_command(line))
-}
-
 int		is_start_command(char *line)
 {
-	return (strequ(line, "##start"));
+	return (ft_strequ(line, "##start"));
 }
 
 int		is_end_command(char *line)
 {
-	return (strequ(line, "##end"));
+	return (ft_strequ(line, "##end"));
+}
+
+int		is_valid_command(char *line)
+{
+	return (is_start_command(line) || is_end_command(line));
+}
+
+int		is_comment(char *line)
+{
+	return (line[0] == '#');
 }
 
 int		str_is_digit(char *str)
@@ -26,60 +26,63 @@ int		str_is_digit(char *str)
 	{
 		if (!ft_isdigit(*str))
 			return (0);
+		str++;
 	}
 	return (1);
 }
 
-int		get_ants_number(char *line)
+void	record_input_line(char **line, t_parsing *data)
+{
+	(void)data; //ajoute un maillon à la liste chainée avec la donnée de la ligne
+	ft_strdel(line);
+}
+
+int		get_ants_number(t_graph *anthill, t_parsing *data)
 {
 	char	*line;
 
 	line = NULL;
 	while (42)
 	{
-		if (get_next_line(0, &line) != 1 || !line)
+		if (get_next_line(0, &line) != 1 || !line
+			|| is_valid_command(line) || ft_strlen(line) > 7)
 		{
-			ft_strdel(&line);
-			return (-1);
-		}
-		if (is_valid_command(line))
-		{
-			ft_strdel(&line);
+			record_input_line(&line, data);
 			return (-1);
 		}
 		if (str_is_digit(line))
-		{	
-			
-			return (0);
+		{
+			anthill->ants_nb = ft_atoi(line);
+			record_input_line(&line, data);
+			return (anthill->ants_nb ? 0 : -1);
 		}
+		else if (!is_comment(line))
+		{
+			record_input_line(&line, data);
+			return (-1);
+		}
+		record_input_line(&line, data);
 	}
 }
 
-int		parser(void)
+int		parser(t_graph *anthill)
 {
-	// char	*line;
-	// int		ret;
+	t_parsing	data;
 
-	// while (42)
-	// {
-	// 	ret = -2;
-	// 	if ((ret = get_next_line(0, &line)) != 1)
-	// 	{
-	// 		ft_strdel(&line);
-	// 		return (0);
-	// 	}
-	// 	else
-	// 	{
-	// 		ft_putendl(line);
-	// 		ft_strdel(&line);
-	// 	}
-	// }
+	if (get_ants_number(anthill, &data) != 0)
+		return (-1);
 	return (0);
 }
 
 int		main(void)
 {
-	if (parser() != 0)
+	t_graph	anthill;
+
+	ft_bzero(&anthill, sizeof(t_graph));
+	if (parser(&anthill) != 0)
+	{
+		ft_putendl("ERROR");
 		return(0);
+	}
 	return (0);
 }

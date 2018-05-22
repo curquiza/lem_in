a92567c773a6692a724f915f7af5d7f39341fdf2
Clurquiz <clementine.urquizar@gmail.com>
2018-05-23 00:30:39 +0200

    upd tests in getting output

--- tests/tests_script.py
+++ tests/tests_script.py
@@ -1,5 +1,9 @@
 import os
+import subprocess
 
 all_files = sorted(os.listdir('maps/'))
 maps = [f for f in all_files if f.endswith('.txt')]
-print maps
+
+for map in maps:
+    output = subprocess.check_output(['./lem-in < maps/' + map], shell=True)
+    print output

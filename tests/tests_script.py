5bdee895e08b22b2f3f614b05b8b4929b0aec88d
Clementine URQUIZAR <curquiza@e2r12p21.42.fr>
2018-05-25 12:01:34 +0200

    reorg test scripts

--- tests/tests_script.py
+++ tests/tests_script.py
@@ -69,48 +69,7 @@ MAPS = {
     'trap_2':                      {'error': False, 'rounds': 4},
 }
 
-def print_rslt(filename, output):
-    if expects_error_msg_for(filename):
-        print_for_error_case(output)
-    else:
-        print_for_algo_case(filename, output)
-
-def manage_error_returns(filename, output, returncode):
-    if returncode != -1 and returncode != 1:
-        print bcolors.FAIL + 'KO' + bcolors.ENDC
-        put('  ' +  output)
-        return
-    if expects_error_msg_for(filename):
-        print_for_error_case(output)
-    elif is_an_error_msg(output):
-        should_not_put_error_msg()
-    else:
-        weird_output_msg(returncode)
-
-def print_for_error_case(output):
-    if is_an_error_msg(output):
-        print bcolors.OKGREEN + 'OK' + bcolors.ENDC
-    else:
-        should_put_error_msg()
-
-def print_for_algo_case(filename, output):
-    if is_an_error_msg(output):
-        should_not_put_error_msg()
-    else:
-        print_rounds_rslt(filename, output)
-
-def print_rounds_rslt(filename, output):
-    if count_rounds(output) == MAPS[filename]['rounds']:
-        print bcolors.OKGREEN + 'OK' + bcolors.ENDC
-    else:
-        print bcolors.FAIL + 'KO' + bcolors.ENDC
-        print "  -> should compute in " + str(MAPS[filename]['rounds']) + " rounds"
-        print "  -> your result : " + str(count_rounds(output)) + " rounds"
-
-def count_rounds(output):
-    lines = output.split('\n')
-    rounds = [l for l in lines if l.startswith('L')]
-    return len(rounds)
+# --- TOOLS ---------------
 
 def expects_error_msg_for(filename):
     return MAPS[filename]['error'] == True
@@ -130,8 +89,63 @@ def weird_output_msg(returncode):
     print bcolors.WARNING + 'WARNING' + bcolors.ENDC
     print "  -> weird output for this return status (" + str(returncode) + ")"
 
+def ok_msg():
+    print bcolors.OKGREEN + 'OK' + bcolors.ENDC
+
+def bad_nb_of_rounds_msg(filename, output):
+    print bcolors.FAIL + 'KO' + bcolors.ENDC
+    print "  -> should compute in " + str(MAPS[filename]['rounds']) + " rounds"
+    print "  -> your result : " + str(count_rounds(output)) + " rounds"
+
+def bad_returncode_msg(output):
+    print bcolors.FAIL + 'KO' + bcolors.ENDC
+    put('  ' +  output)
+    
+
 def put(str):
     sys.stdout.write(str)
+
+# --- SUB-FUNCTIONS -------
+
+def print_rslt(filename, output):
+    if expects_error_msg_for(filename):
+        print_for_error_case(output)
+    else:
+        print_for_algo_case(filename, output)
+
+def manage_error_returns(filename, output, returncode):
+    if returncode != -1 and returncode != 1:
+        bad_returncode_msg(output)
+        return
+    if expects_error_msg_for(filename):
+        print_for_error_case(output)
+    elif is_an_error_msg(output):
+        should_not_put_error_msg()
+    else:
+        weird_output_msg(returncode)
+
+def print_for_error_case(output):
+    if is_an_error_msg(output):
+        ok_msg()
+    else:
+        should_put_error_msg()
+
+def print_for_algo_case(filename, output):
+    if is_an_error_msg(output):
+        should_not_put_error_msg()
+    else:
+        print_rounds_rslt(filename, output)
+
+def print_rounds_rslt(filename, output):
+    if count_rounds(output) == MAPS[filename]['rounds']:
+        ok_msg()
+    else:
+        bad_nb_of_rounds_msg(filename, output)
+
+def count_rounds(output):
+    lines = output.split('\n')
+    rounds = [l for l in lines if l.startswith('L')]
+    return len(rounds)
 
 # --- MAIN ----------------
 

5495898a38d3ff06382b9d53a25492b1b0b5c9bd
curquiza <clementine.urquizar@gmail.com>
2018-05-26 18:15:15 +0200

    add \n on record_input

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -9,11 +9,12 @@ void	record_input_line(char **line, t_parsing *data)
 	line_len = *line ? ft_strlen(*line) : 0;
 	old_len = data->input ? ft_strlen(data->input) : 0;
 	buff = data->input;
-	data->input = ft_strnew(old_len + line_len);
+	data->input = ft_strnew(old_len + line_len + 1);
 	if (buff)
 		ft_strcpy(data->input, buff);
 	if (*line)
 		ft_strcpy(data->input + old_len, *line);
+	data->input[old_len + line_len] = '\n';
 	ft_strdel(line);
 	ft_strdel(&buff);
 }

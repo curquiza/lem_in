4dcee654bebb700d1464d97a66da3f800a1fba73
curquiza <clementine.urquizar@gmail.com>
2018-05-26 17:22:38 +0200

    add functions to add room in the list

--- srcs/add_rooms.c
+++ srcs/add_rooms.c
@@ -1,25 +1,42 @@
 #include "lem-in.h"
 
-// void	ft_lstadd(t_list **alst, t_list *new)
-// {
-// 	if (alst == NULL)
-// 		return ;
-// 	else if (*alst == NULL)
-// 		*alst = new;
-// 	else
-// 	{
-// 		new->next = *alst;
-// 		*alst = new;
-// 	}
-// }
-
-void	add_room_to_anthill(char *line, t_graph *anthill, int special_room)
+void	ft_room_lstadd(t_room **alst, t_room *new)
 {
-	(void)line;
-	(void)anthill;
-	(void)special_room;
+	if (!alst)
+		return ;
+	else if (*alst == NULL)
+		*alst = new;
+	else
+	{
+		new->next = *alst;
+		*alst = new;
+	}
+}
 
-	//ajouter un maillon à la liste chainée des rooms
+t_room	*ft_room_lstnew(char *name, int special_room, int index)
+{
+	t_room		*new;
+
+	if (!(new = (t_room *)malloc(sizeof(*new))))
+		ft_exit("malloc error", 1);
+	ft_bzero(new, sizeof(*new));
+	new->name = ft_strdup(name);
+	new->special_room = special_room;
+	new->id = index;
+	new->next = NULL;
+	return (new);
+}
+
+void	add_room_to_anthill(char *line, t_graph *anthill, t_parsing *data, int special_room)
+{
+	char	*name;
+	char 	**infos;
+
+	infos = ft_strsplit(line, ' ');
+	name = infos[0];
+	ft_room_lstadd(&anthill->rooms_list, ft_room_lstnew(name, special_room, data->rooms_nb));
+	data->rooms_nb += 1;
+	ft_tabdel(&infos);
 }
 
 

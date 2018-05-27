dffb9f42168a083cfba06c273d006ae364cc487c
Clurquiz <clementine.urquizar@gmail.com>
2018-05-27 14:21:38 +0200

    add del functions

--- srcs/del_anthill.c
+++ srcs/del_anthill.c
@@ -1,6 +1,49 @@
 #include "lem-in.h"
 
-void	del_anthill(t_graph *anthill)
+static void	ft_del_adj_matrix(int ***tab, int rooms_nb)
 {
-	(void)anthill;
+	int		i;
+
+	if (!tab || !(*tab))
+		return ;
+	i = 0;
+	while (i < rooms_nb)
+	{
+		free((*tab)[i]);
+		(*tab)[i] = NULL;
+		i++;
+	}
+	free(*tab);
+	*tab = NULL;
+}
+
+static void	ft_del_room(t_room **room)
+{
+	ft_strdel(&(*room)->name);
+	ft_bzero(*room, sizeof(*room));
+	free(*room);
+	*room = NULL;
+}
+
+static void	ft_del_rooms_array(t_room ***tab, int rooms_nb)
+{
+	int		i;
+
+	if (!tab || !(*tab))
+		return ;
+	i = 0;
+	while (i < rooms_nb)
+	{
+		ft_del_room(&(*tab)[i]);
+		i++;
+	}
+	free(*tab);
+	*tab = NULL;
+}
+
+void		del_anthill(t_graph *anthill)
+{
+	ft_del_adj_matrix(&anthill->adj_matrix, anthill->rooms_nb);
+	ft_del_rooms_array(&anthill->rooms_array, anthill->rooms_nb);
+	ft_bzero(anthill, sizeof(*anthill));
 }

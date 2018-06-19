6a581c66860ff3055bb4cac68a9202d2ba637381
curquiza <clementine.urquizar@gmail.com>
2018-06-19 09:33:14 +0200

    upd norm of main algo file

--- srcs/algo.c
+++ srcs/algo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/19 09:32:47 by curquiza          #+#    #+#             */
+/*   Updated: 2018/06/19 09:32:58 by curquiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lem_in.h"
 
 static t_room	*switch_rooms(t_room *room_1, t_room *rooms_2)
@@ -22,7 +34,7 @@ static t_room	*sort_rooms(t_room *rooms)
 	return (rooms);
 }
 
-void	algo(t_graph *anthill)
+void			algo(t_graph *anthill)
 {
 	anthill->rooms_list = sort_rooms(anthill->rooms_list);
 	anthill->ants_in_start = anthill->ants_nb;
